f1a252c129063764f03bfd83f546ebebac9ab198
FranpokVEVO <francis.jorgemoner@gmail.com>
2025-10-06 10:15:21 -0300

    e

--- JUEGO 1 (FLAPY BIRD PRIMERA PERSONA) git/Assets/scripts/FLAPY BIRD/puntos.cs
+++ JUEGO 1 (FLAPY BIRD PRIMERA PERSONA) git/Assets/scripts/FLAPY BIRD/puntos.cs
@@ -1,24 +1,23 @@
 using UnityEngine;
+using TMPro;
 
-public class PuntosTrigger : MonoBehaviour
+public class SumarPuntos : MonoBehaviour
 {
     public int puntos = 1;
-    public static int puntajeTotal = 0;
-    public TextMesh textoPuntaje; // Arrastrar desde inspector
+    private TextMeshProUGUI textoPuntaje;
+    private static long puntajeTotal = 0;
 
-    private void OnTriggerEnter(Collider other)
+    void Start()
+    {
+        textoPuntaje = GameObject.FindGameObjectWithTag("Puntaje").GetComponent<TextMeshProUGUI>();
+    }
+
+    void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             puntajeTotal += puntos;
-
-            if (textoPuntaje != null)
-                textoPuntaje.text = "Puntaje: " + puntajeTotal;
-
-            Debug.Log("Jugador pasó y sumó puntos: " + puntajeTotal);
-
-            // Destruir solo el trigger para no sumar dos veces
-            Destroy(gameObject);
+            textoPuntaje.text = "Puntos: " + puntajeTotal;
         }
     }
 }
