80c3da55e44d4998b8ae5e851291cf96cffc3f9f
FranpokVEVO <francis.jorgemoner@gmail.com>
2025-10-06 19:33:54 -0300

    e

--- JUEGO 1 (FLAPY BIRD PRIMERA PERSONA) git/Assets/scripts/FLAPY BIRD/MovimientoJugador.cs
+++ JUEGO 1 (FLAPY BIRD PRIMERA PERSONA) git/Assets/scripts/FLAPY BIRD/MovimientoJugador.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class MovimientoJugador : MonoBehaviour
 {
-    public float speed = 5f;
     public float salto = 5f;
     public float adelante = 5f;
 
@@ -17,11 +16,6 @@ public class MovimientoJugador : MonoBehaviour
     {
       
         transform.Translate(Vector3.forward * adelante * Time.deltaTime);
-
-        
-        float horizontal = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector3(horizontal * speed, rb.velocity.y, rb.velocity.z);
-
        
         if (Input.GetKeyDown(KeyCode.Space))
         {
--- JUEGO 1 (FLAPY BIRD PRIMERA PERSONA) git/Assets/scripts/FLAPY BIRD/TECHO Y PISO (LIMITE).cs
+++ JUEGO 1 (FLAPY BIRD PRIMERA PERSONA) git/Assets/scripts/FLAPY BIRD/TECHO Y PISO (LIMITE).cs
@@ -2,22 +2,9 @@ using UnityEngine;
 
 public class TECHO_Y_PISO_LIMITE : MonoBehaviour
 {
- public Transform player;
-    public float pisoY = 0f;
-    public float techoY = 12f;
-
+    public float velocidadZ = 5f;
     void Update()
     {
-        if (player == null) return;
-
-      
-        if (gameObject.name == "PISO 2")
-        {
-            transform.position = new Vector3(player.position.x, pisoY, player.position.z);
-        }
-        else if (gameObject.name == "PISO 1")
-        {
-            transform.position = new Vector3(player.position.x, techoY, player.position.z);
-        }
+         transform.position += new Vector3(0f, 0f, velocidadZ * Time.deltaTime);
     }
 }
